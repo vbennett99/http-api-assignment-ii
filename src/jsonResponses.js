0353d16e9e7f8049c18a6131794cc07148697d37
Vincent Bennett <vmbennett99@gmail.com>
2020-09-20 16:34:05 -0400

    Started assignment, took starter files from POST class exercise and modified to add get/head capability

---
+++ src/jsonResponses.js
@@ -0,0 +1,73 @@
+// Note this object is purely in memory
+// When node shuts down this will be cleared.
+// Same when your heroku app shuts down from inactivity
+// We will be working with databases in the next few weeks.
+const users = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+    users[body.name].name = body.name;
+  }
+
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully!';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you were looking for is not found.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  addUser,
+  notFound,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/updateUser': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const body = [];
+
+    request.on('error', (err) => { // Return bad request on error
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    request.on('data', (chunk) => { // Add any data chunks to our body array
+      body.push(chunk);
+    });
+
+    request.on('end', () => { // Once all the data is received, make it readable for the jsonHandler to process
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addUser(request, response, bodyParams);
+    });
+  }
+};
+
+const handleGetAndHead = (request, response, parsedUrl) => {
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  console.dir(parsedUrl.pathname);
+  console.dir(request.method);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGetAndHead(request, response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1:${port}`);
